401ba268ebdd681b808d774b7baf344f54f6a9f2
Haapy <haapy@air-de-haapy.home>
2025-02-26 19:18:29 +0100

    edited pong rules

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,19 +19,31 @@ function closeLeaderboard() {
 }
 
 function loadTemplate() {
-	const templates = [
-		{ url: 'assets/templates/leaderboard.html', elementId: 'leaderboard-popup' },
-		{ url: 'assets/templates/navbar.html', elementId: 'navbar-container' }
-	];
+    const templates = [
+        { url: 'assets/templates/leaderboard.html', elementId: 'leaderboard-popup' },
+        { url: 'assets/templates/navbar.html', elementId: 'navbar-container' }
+    ];
 
     const fetchPromises = templates.map(template =>
-        fetch(template.url).then(response => response.text())
+        fetch(template.url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch ${template.url}: ${response.statusText}`);
+                }
+                return response.text();
+            })
     );
 
     Promise.all(fetchPromises)
         .then(dataArray => {
             dataArray.forEach((data, index) => {
-                document.getElementById(templates[index].elementId).innerHTML = data;
+                const element = document.getElementById(templates[index].elementId);
+                if (element) {
+                    console.log(`Inserting content into #${templates[index].elementId}`);
+                    element.innerHTML = data;
+                } else {
+                    console.error(`Element with ID ${templates[index].elementId} not found`);
+                }
             });
         })
         .catch(error => {
