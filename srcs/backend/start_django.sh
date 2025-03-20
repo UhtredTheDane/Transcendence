e9e76ab0283261232b1e5e2bc6165a65094ecabf
Yanis AINOUCHE <yaainouc@paul-f4Ar7s5.clusters.42paris.fr>
2025-03-20 16:24:10 +0100

    Uvicorn

--- srcs/backend/start_django.sh
+++ srcs/backend/start_django.sh
@@ -3,4 +3,4 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput --username admin --email bidon93@live.fr
 python manage.py createsuperuser --noinput --username admin2 --email bidon42@gmail.com
-python manage.py runserver 0.0.0.0:8000
+uvicorn base.asgi:application --host 0.0.0.0 --port 8000 --reload --ssl-keyfile=/etc/ssl/private/server.key --ssl-certfile=/etc/ssl/certs/server.crt 
