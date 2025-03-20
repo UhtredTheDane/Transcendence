86894788151094ac434fbe1c5980a79859f6cb36
Sofian Elaloui <soelalou@paul-f4Ar1s2.clusters.42paris.fr>
2025-03-20 13:50:22 +0100

    remove redis from settings

--- srcs/backend/base/settings.py
+++ srcs/backend/base/settings.py
@@ -182,23 +182,9 @@ SOCIALACCOUNT_QUERY_EMAIL = True
 
 CHANNEL_LAYERS = {
 	'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('redis', 6379)],
-        },
-    },
+		'BACKEND': 'channels.layers.InMemoryChannelLayer',  # Utilise le bon backend
+	},
 }
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
